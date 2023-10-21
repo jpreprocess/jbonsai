663947c4c71507403fcdbac78e86f4d74ef27748
femshima <49227365+femshima@users.noreply.github.com>
2023-10-21 16:49:10 +0900

    main to lib

--- src/lib.rs
+++ src/lib.rs
@@ -9,6 +9,15 @@ mod HTS_vocoder;
 
 mod util;
 
+pub use HTS_engine::*;
+pub use HTS_gstream::*;
+pub use HTS_label::*;
+pub use HTS_misc::*;
+pub use HTS_model::*;
+pub use HTS_pstream::*;
+pub use HTS_sstream::*;
+pub use HTS_vocoder::*;
+
 fn main() {
     println!("Hello, world!");
 }
