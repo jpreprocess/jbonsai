de7858a02a340e31a33454c140d260e9976c9d74
femshima <49227365+femshima@users.noreply.github.com>
2024-01-26 21:37:09 +0900

    start stream parser

--- src/model/parser/header_serde.rs
+++ src/model/parser/header_serde.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::fmt::{self, Display};
 use std::ops::{AddAssign, MulAssign};
 
@@ -121,11 +122,11 @@ impl<'de> Deserializer<'de> {
 impl<'de, 'a> de::Deserializer<'de> for &'a mut Deserializer<'de> {
     type Error = Error;
 
-    fn deserialize_any<V>(self, _visitor: V) -> Result<V::Value>
+    fn deserialize_any<V>(self, visitor: V) -> Result<V::Value>
     where
         V: Visitor<'de>,
     {
-        unimplemented!()
+        self.deserialize_str(visitor)
     }
 
     forward_to_deserialize_any! {
@@ -299,6 +300,7 @@ impl<'de, 'a> MapAccess<'de> for NewlineSeparated<'a, 'de> {
     where
         K: DeserializeSeed<'de>,
     {
+        dbg!(&self.de.input);
         let mut newline = false;
         loop {
             let char = self.de.peek_char();
@@ -328,29 +330,72 @@ impl<'de, 'a> MapAccess<'de> for NewlineSeparated<'a, 'de> {
 }
 
 ////////////////////////////////////////////////////////////////////////////////
+#[derive(Deserialize, PartialEq, Debug)]
+#[serde(rename_all = "SCREAMING_SNAKE_CASE")]
+struct Test {
+    fullcontext_version: String,
+    gv_off_context: Vec<String>,
+    sampling_frequency: usize,
+    stream_win: Vec<(usize, usize)>,
+    #[serde(flatten, with = "with")]
+    test: HashMap<String, TestInner>,
+}
+
+#[derive(Deserialize, PartialEq, Debug)]
+#[serde(rename_all = "SCREAMING_SNAKE_CASE")]
+struct TestInner {
+    stream_pdf: (usize, usize),
+}
 
 #[test]
 fn test_struct() {
-    #[derive(Deserialize, PartialEq, Debug)]
-    #[serde(rename_all = "SCREAMING_SNAKE_CASE")]
-    struct Test {
-        fullcontext_version: String,
-        gv_off_context: Vec<String>,
-        sampling_frequency: usize,
-        stream_win: Vec<(usize, usize)>,
-    }
-
     let j = r#"
 FULLCONTEXT_VERSION:1.0
 GV_OFF_CONTEXT:"*-sil+*","*-pau+*"
 SAMPLING_FREQUENCY:48000
 STREAM_WIN:40880-40885,40886-40900
+STREAM_PDF[LF0]:788578-848853
 "#;
     let expected = Test {
         fullcontext_version: "1.0".to_string(),
         gv_off_context: vec!["*-sil+*".to_owned(), "*-pau+*".to_owned()],
         sampling_frequency: 48000,
         stream_win: vec![(40880, 40885), (40886, 40900)],
+        test: HashMap::new(),
     };
     assert_eq!(expected, from_str(j).unwrap());
+}
+
+mod with {
+    use std::collections::HashMap;
+
+    use serde::{de::Visitor, Deserialize, Deserializer};
+
+    pub fn deserialize<'de, D, T>(deserializer: D) -> Result<HashMap<String, T>, D::Error>
+    where
+        D: Deserializer<'de>,
+        T: Deserialize<'de> + std::fmt::Debug,
+    {
+        struct StrMapVisitor;
+        impl<'de> Visitor<'de> for StrMapVisitor {
+            type Value = Vec<(String, String)>;
+            fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
+                formatter.write_str("StrMap")
+            }
+            fn visit_map<A>(self, mut map: A) -> Result<Self::Value, A::Error>
+            where
+                A: serde::de::MapAccess<'de>,
+            {
+                let mut result = Vec::new();
+                while let Some(entry) = map.next_entry()? {
+                    result.push(entry);
+                }
+                Ok(result)
+            }
+        }
+
+        let s = deserializer.deserialize_map(StrMapVisitor)?;
+        dbg!(s);
+        todo!()
+    }
 }
