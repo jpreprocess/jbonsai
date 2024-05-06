26e05f89990584d4ac167868c62a6325d7e829e0
cm-ayf <cm.ayf2734@gmail.com>
2024-05-06 14:12:35 +0900

    refactor 3

--- examples/genji/main.rs
+++ examples/genji/main.rs
@@ -1,18 +1,18 @@
-use jbonsai::{engine::Engine, model::interporation_weight::Weights};
+use jbonsai::engine::Engine;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let label_str = std::fs::read_to_string("examples/genji/genji.lab")?;
 
-    let lines: Vec<String> = label_str.lines().map(|s| s.to_string()).collect();
-    let mut engine = Engine::load(&vec![
+    let lines: Vec<_> = label_str.lines().collect();
+    let mut engine = Engine::load(&[
         "models/tohoku-f01/tohoku-f01-sad.htsvoice",
         "models/tohoku-f01/tohoku-f01-happy.htsvoice",
     ])?;
     let iw = engine.condition.get_interporation_weight_mut();
-    iw.set_duration(Weights::new(&[0.5, 0.5])?)?;
-    iw.set_parameter(0, Weights::new(&[0.5, 0.5])?)?;
-    iw.set_parameter(1, Weights::new(&[0.5, 0.5])?)?;
-    iw.set_parameter(2, Weights::new(&[1.0, 0.0])?)?;
+    iw.set_duration(&[0.5, 0.5])?;
+    iw.set_parameter(0, &[0.5, 0.5])?;
+    iw.set_parameter(1, &[0.5, 0.5])?;
+    iw.set_parameter(2, &[1.0, 0.0])?;
 
     let speech = engine.synthesize_from_strings(&lines)?;
 
