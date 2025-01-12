76acc05bef0793c4f474ddbe8385da14ecad344a
cm-ayf <cm.ayf2734@gmail.com>
2025-01-13 08:32:06 +0900

    refactor: mlsa optimize

--- src/vocoder/mlsa.rs
+++ src/vocoder/mlsa.rs
@@ -81,14 +81,29 @@ impl<const N: usize> MelLogSpectrumApproximation<N> {
         self.d2[0][0] = Self::df_apply(&self.d2, x);
     }
 
+    // calculate d[t] from d[t-1] such that
+    // - d[t][0] = alpha d[t-1][0]
+    // - d[t][i] = d[t-1][i-1] + alpha (d[t-1][i] - dr[t][i-1])
+    //
+    // using rem(i) := d[t-1][i-1] - alpha d[t][i-1], we have
+    // - d[t][i]  = alpha d[t-1][i] + rem(i)
+    // - rem(1)   = d[t-1][0] - alpha d[t][0]
+    //            = (1 - alpha^2) d[t-1][0]
+    // - rem(i+1) = d[t-1][i] - alpha d[t][i]
+    //            = (1 - alpha^2) d[t-1][i] - alpha rem(i)
+    //
+    // needless_range_loop for better understanding along with the explanation
     #[inline(always)]
+    #[allow(clippy::needless_range_loop)]
     fn df2_step1(d: &mut [f64], alpha: f64) {
-        let mut prev = alpha * d[0];
-        for i in 0..d.len() - 1 {
-            d[i] += alpha * (d[i + 1] - prev);
-            (d[i], prev) = (prev, d[i]);
+        let aa = 1.0 - alpha * alpha;
+        // skip d[t][0] as it is never used
+        // `rem` value used in loop of i = rem(i)
+        let mut rem = aa * d[0]; // rem(1)
+        for i in 1..d.len() {
+            // calculate d[t][i] and rem(i+1)
+            (d[i], rem) = (alpha * d[i] + rem, aa * d[i] - alpha * rem);
         }
-        d[d.len() - 1] = prev;
     }
 
     #[inline(always)]
