86441a40b74780afef9aa9901ac5602ffd8788fd
femshima <49227365+femshima@users.noreply.github.com>
2026-06-14 23:33:00 +0900

    improve the script

--- hts-bench/bench.sh
+++ hts-bench/bench.sh
@@ -8,6 +8,21 @@ cd "$SCRIPT_DIR"
 
 rm -rf builds hts_engine_API || true
 mkdir -p builds
+mkdir -p "$SCRIPT_DIR/results"
+
+echo "## CPU" > "$SCRIPT_DIR/results/system_info.txt"
+lscpu >> "$SCRIPT_DIR/results/system_info.txt"
+echo "## Memory" >> "$SCRIPT_DIR/results/system_info.txt"
+sudo free -h >> "$SCRIPT_DIR/results/system_info.txt"
+echo "## OS" >> "$SCRIPT_DIR/results/system_info.txt"
+cat /etc/os-release >> "$SCRIPT_DIR/results/system_info.txt"
+uname -rsv >> "$SCRIPT_DIR/results/system_info.txt"
+echo "## Compiler Versions" >> "$SCRIPT_DIR/results/system_info.txt"
+ldd --version | head -n 1 >> "$SCRIPT_DIR/results/system_info.txt"
+clang-22 --version >> "$SCRIPT_DIR/results/system_info.txt"
+rustc --version --verbose >> "$SCRIPT_DIR/results/system_info.txt"
+echo "## Hyperfine Version" >> "$SCRIPT_DIR/results/system_info.txt"
+hyperfine --version >> "$SCRIPT_DIR/results/system_info.txt"
 
 git clone https://github.com/jpreprocess/hts_engine_API.git
 cd hts_engine_API
@@ -34,11 +49,11 @@ EOF
 mkdir -p src/build
 cd src/build
 
-cmake -DCMAKE_BUILD_TYPE=Release -DCPU_NATIVE=OFF ..
+CC=clang-22 CXX=clang-22 cmake -DCMAKE_BUILD_TYPE=Release -DCPU_NATIVE=OFF ..
 make -j1
 cp bin/hts_engine "$SCRIPT_DIR/builds/hts_engine_default"
 
-cmake -DCMAKE_BUILD_TYPE=Release -DCPU_NATIVE=ON ..
+CC=clang-22 CXX=clang-22 cmake -DCMAKE_BUILD_TYPE=Release -DCPU_NATIVE=ON ..
 make -j1
 cp bin/hts_engine "$SCRIPT_DIR/builds/hts_engine_native"
 
@@ -53,7 +68,13 @@ cp target/release/examples/constitution "$SCRIPT_DIR/builds/jbonsai_lto"
 RUSTFLAGS="-C codegen-units=1 -C linker-plugin-lto=on -C linker=clang-22 -C link-arg=-fuse-ld=lld-22 -C target-cpu=native" cargo build --example constitution --features=binary --release
 cp target/release/examples/constitution "$SCRIPT_DIR/builds/jbonsai_native"
 
-mkdir -p "$SCRIPT_DIR/results"
+"$SCRIPT_DIR/builds/hts_engine_native" > "$SCRIPT_DIR/results/hts_engine.raw"
+"$SCRIPT_DIR/builds/jbonsai_native" > "$SCRIPT_DIR/results/jbonsai.raw"
+
+cmp --silent "$SCRIPT_DIR/results/hts_engine.raw" "$SCRIPT_DIR/results/jbonsai.raw" && echo "Outputs match!" || echo "Output mismatch between HTS Engine and jbonsai!"
+
+echo "## CPU Scaling Governors" >> "$SCRIPT_DIR/results/system_info.txt"
+cat /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor | sort | uniq >> "$SCRIPT_DIR/results/system_info.txt"
 
 hyperfine --warmup 5 --runs 25 \
     --export-json "$SCRIPT_DIR/results/benchmark.json" \
@@ -63,12 +84,6 @@ hyperfine --warmup 5 --runs 25 \
     "$SCRIPT_DIR/builds/jbonsai_lto" \
     "$SCRIPT_DIR/builds/jbonsai_native"
 
-"$SCRIPT_DIR/builds/hts_engine_native" > "$SCRIPT_DIR/results/hts_engine.raw"
-"$SCRIPT_DIR/builds/jbonsai_native" > "$SCRIPT_DIR/results/jbonsai.raw"
-
-echo $(uname -a) > "$SCRIPT_DIR/results/system_info.txt"
-cat /proc/cpuinfo | grep "model name" | head -n 1 >> "$SCRIPT_DIR/results/system_info.txt"
-rustc -vV >> "$SCRIPT_DIR/results/system_info.txt"
 echo "HTS Engine API commit: $(git -C "$SCRIPT_DIR/hts_engine_API" rev-parse HEAD)" > "$SCRIPT_DIR/results/hts_engine_commit.txt"
 echo "JBonsai commit: $(git -C "$PROJECT_ROOT" rev-parse HEAD)" > "$SCRIPT_DIR/results/jbonsai_commit.txt"
 
